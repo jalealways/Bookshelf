7ae7f5cee719eedf6b89dd2af9fa30dce2ffcf75
xujale <jale_me@163.com>
2017-07-04 21:49:24 +0800

    commit

--- bookshelf/apps/borrow_books/services/borrowService.py
+++ bookshelf/apps/borrow_books/services/borrowService.py
@@ -2,8 +2,8 @@ from .. import models
 import uuid
 
 
-def borrow_num(oppenid):
-    re = models.TbReaderInfo.filter(wechat_id=oppenid)
+def borrow_num(oppen_id):
+    re = models.TbReaderInfo.filter(wechat_id=oppen_id)
     if re.exists():
         if re[0].sessionid:
             return 'duang'
@@ -14,4 +14,21 @@ def borrow_num(oppenid):
         return 'register'
 
 
-def
+def box_status(box_id, oppen_id):
+    box = models.TbBookshelfBoxInfo.filter(box_id=box_id)
+    book_id, box_status, lock_status,  ray_status, \
+    lock_board_id, lock_id, raspberry_ip, order_reader_id =  \
+        box.book_id, box.box_status, box.lock_status,  \
+        box.ray_status, box.lock_board_id, box.lock_id, \
+        box.raspberry_ip, box.order_reader_id
+    if box_status == '1':
+        msg = "异常"
+        return msg
+    elif oppen_id != order_reader_id:
+        return "别人预约"
+    elif "unlock" == lock_status:
+        return "门没锁"
+    elif "0" == ray_status:
+        return "书没了"
+    else:
+        return (box_id, "操作命令", lock_board_id, lock_id)
--- bookshelf/apps/borrow_books/views.py
+++ bookshelf/apps/borrow_books/views.py
@@ -1,22 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import sys
 from django.http import HttpResponse
-from service import borrowService as service
-from .. import untils_
-
+from services import borrowService as service
 
 
 def load(request):
     oppen_id = ''
+    box_id = ''
     if oppen_id:
-        res = service.brow_num(oppen_id)
-        if res == 'register':
-            return HttpResponse('注册.html')
-        elif res == 'duang':
-            return HttpResponse('先把其他流程完成.html')
-        else:
-            return res
+        return user_auth(oppen_id, box_id)
     else:
         return HttpResponse('index.html')
 
+
+def user_auth(oppen_id, box_id):
+    res = service.brow_num(oppen_id)
+    if res == 'register':
+        return HttpResponse('注册.html')
+    elif res == 'duang':
+        return HttpResponse('先把其他流程完成.html')
+    else:
+        borrow_num = res
+        return box_status(box_id, oppen_id, borrow_num)
+
+
+def box_status(box_id, oppen_id):
+    res_msg = service.box_status(box_id, oppen_id)
+    pass
+    return res_msg
+
+def unlock(request):
+    pass
+
+
+def back_book_monitor(request):
+    pass
