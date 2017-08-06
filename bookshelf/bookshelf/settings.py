a511687a1d20d4b6e54c30cc532aff9fe17ccabc
xujale <jale_me@163.com>
2017-08-06 21:07:52 +0800

    commit

--- bookshelf/bookshelf/settings.py
+++ bookshelf/bookshelf/settings.py
@@ -125,7 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/test/static/'
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'mp')
+]
+
+
+STATIC_URL = '/'
+STATIC_ROOT = '/var/www/bookshelf/static/'
 
