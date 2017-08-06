630dfccac2a24b51d8b0b32f70cbdb154ec9b382
xujale <jale_me@163.com>
2017-08-06 21:49:46 +0800

    commit

--- bookshelf/apps/borrow_books/views.py
+++ bookshelf/apps/borrow_books/views.py
@@ -12,38 +12,38 @@ def load(request):
     with open('x.txt', 'w') as f:
         f.write(x)
     return HttpResponse(x)
-    oppen_id = '1'
-    box_id = 'box0000003'
-
-    #  验证是否存在oppen_id
-    oppen_id_exits = service.verify_oppen_id(oppen_id)
-    if oppen_id_exits:
-        #  用户权限校验
-        user_rights = service.user_rights_check(oppen_id, box_id)
-        if user_rights == '有任务正在进行':
-            return HttpResponse("有任务.html")
-        elif user_rights == '超出可借数量':
-            return HttpResponse('超出可借数量')
-        else:
-            #  隔间状态校验
-            box_status = service.box_status_(box_id, oppen_id)
-            if box_status == '被占用':
-                return HttpResponse('提示：被占用')
-            elif box_status == '门没锁':
-                return HttpResponse('提示：门没锁')
-            elif box_status == '书没了':
-                return HttpResponse('提示：没有书在里面')
-            else:
-                #  开锁
-                msg = service.unlock(box_status)
-                if msg == '异常':
-                    return HttpResponse('开锁出现异常')
-                else:
-                    return 'ok'
-
-    else:
-        return HttpResponse('注册.html')
-
-
-def back_book_monitor(request):
-    pass
+#     oppen_id = '1'
+#     box_id = 'box0000003'
+#
+#     #  验证是否存在oppen_id
+#     oppen_id_exits = service.verify_oppen_id(oppen_id)
+#     if oppen_id_exits:
+#         #  用户权限校验
+#         user_rights = service.user_rights_check(oppen_id, box_id)
+#         if user_rights == '有任务正在进行':
+#             return HttpResponse("有任务.html")
+#         elif user_rights == '超出可借数量':
+#             return HttpResponse('超出可借数量')
+#         else:
+#             #  隔间状态校验
+#             box_status = service.box_status_(box_id, oppen_id)
+#             if box_status == '被占用':
+#                 return HttpResponse('提示：被占用')
+#             elif box_status == '门没锁':
+#                 return HttpResponse('提示：门没锁')
+#             elif box_status == '书没了':
+#                 return HttpResponse('提示：没有书在里面')
+#             else:
+#                 #  开锁
+#                 msg = service.unlock(box_status)
+#                 if msg == '异常':
+#                     return HttpResponse('开锁出现异常')
+#                 else:
+#                     return 'ok'
+#
+#     else:
+#         return HttpResponse('注册.html')
+#
+#
+# def back_book_monitor(request):
+#     pass
