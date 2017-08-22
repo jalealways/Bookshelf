ae0702cccba0e33eb06c6e7b55320eaeae98ed9a
jalealways <jale_me@163.com>
2017-08-22 20:40:30 +0800

    commit

--- bookshelf/apps/borrow_books/services/borrowService.py
+++ bookshelf/apps/borrow_books/services/borrowService.py
@@ -49,7 +49,7 @@ def box_status_(box_id, oppen_id):
         obj.update(box_status='0')
         return "book_none"
     else:
-        return [(board_type, board_path, lock_board_id, lock_id, 'unlock', 1, 1),
+        return [(board_type, board_path, lock_board_id, lock_id, 'Unlock', 1, 1),
                 (board_type, board_path, lock_board_id, lock_id, 'check', 1, 1),
                 raspberry_ip]
 
