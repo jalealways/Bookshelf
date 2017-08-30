2f8eed82ee5f505fd245562e5a8ab339be462e6a
jalealways <jale_me@163.com>
2017-08-30 13:52:14 +0800

    commit

--- bookshelf/apps/register_login/views.py
+++ bookshelf/apps/register_login/views.py
@@ -19,7 +19,7 @@ def regist(request):
     if id:
         openid = id_
 
-    req = simplejson.loads(request.POST)
+    req = simplejson.loads(request.body)
     password = req.get("password", 0)
     tel = req.get('tel', 0)
     if not password or not tel:
@@ -38,7 +38,7 @@ def regist(request):
 
 def login(request):
 
-    req = simplejson.loads(request.POST)
+    req = simplejson.loads(request.body)
     tel = req.get('tel')
     password = req.get('password')
     if not password or not tel:
